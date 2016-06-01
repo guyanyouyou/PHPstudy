c3c423d3ef3617dea6d62224c3e39fa916fde8cd
孤雁幽幽 <guyanyouyou@163.com>
2016-06-01 23:49:54 +0800

    HTTP协议概念和工作流程

---
+++ http/01.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * @Author: 孤雁幽幽
+ * @Date:   2016-05-30 22:28:50
+ * @Last Modified by:   孤雁幽幽
+ * @Last Modified time: 2016-06-01 23:18:02
+ */
+echo "hello";
---
+++ http/02.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @Author: 孤雁幽幽
+ * @Date:   2016-06-01 23:29:53
+ * @Last Modified by:   孤雁幽幽
+ * @Last Modified time: 2016-06-01 23:40:34
+ */
+//这个程序是把收到的数据，写入文本
+//要求用telnet来请求它
+//
+//分析：要用POST方法
+//$方法 $路径 $版本
+//请求行……
+//主体内容……
+//据上：
+/*POST /oop/PHPstudy/http/02.php HTTP/1.1
+Host:localhost
+Content-length:23
+
+username=zhangsan&age=28*/
+//
+$str = implode($_POST,"n");
+file_put_contents('./post.txt', $str);
+echo 'write ok';
