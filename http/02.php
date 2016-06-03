e674763664a4ef40fe9ddcd047ab2a3145d6196f
孤雁幽幽 <guyanyouyou@163.com>
2016-06-03 23:18:56 +0800

    http协议之方法与状态码

--- http/02.php
+++ http/02.php
@@ -3,7 +3,7 @@
  * @Author: 孤雁幽幽
  * @Date:   2016-06-01 23:29:53
  * @Last Modified by:   孤雁幽幽
- * @Last Modified time: 2016-06-01 23:40:34
+ * @Last Modified time: 2016-06-01 23:50:44
  */
 //这个程序是把收到的数据，写入文本
 //要求用telnet来请求它
@@ -16,6 +16,7 @@
 /*POST /oop/PHPstudy/http/02.php HTTP/1.1
 Host:localhost
 Content-length:23
+Content-type:application/x-www-form-urlencoded
 
 username=zhangsan&age=28*/
 //
